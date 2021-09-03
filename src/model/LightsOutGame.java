ea6d6773e34a9b17a4b7d564c6d9f102465f87b4
Cristian Granero <cristian_granero90@yahoo.com>
2021-09-03 20:27:05 -0300

    initial commit

---
+++ src/model/LightsOutGame.java
@@ -0,0 +1,109 @@
+package model;
+
+public class LightsOutGame {
+
+	private boolean board[][] ;
+	private int movements;
+	
+	public LightsOutGame(int size) {
+		
+		checkSizeValue(size);
+		board = new boolean[size][size];
+		this.movements = 0;
+	}
+	
+	public void generateLights(int row, int column) {
+
+		if(!checkNumbers(row, column)) {
+			addLight(row, column);
+		}
+		if(!checkNumbers(row-1, column)) {
+			addLight(row-1, column);
+		}
+		if(!checkNumbers(row+1, column)) {
+			addLight(row+1, column);
+		}
+		if(!checkNumbers(row, column+1)) {
+			addLight(row, column+1);
+		}
+		if(!checkNumbers(row, column-1)) {
+			addLight(row, column-1);
+		}		
+	}
+	
+	public void removeLights(int row, int column) {
+		if(!checkNumbers(row, column)) {
+			removeLight(row, column);
+		}
+		if(!checkNumbers(row-1, column)) {
+			removeLight(row-1, column);
+		}
+		if(!checkNumbers(row+1, column)) {
+			removeLight(row+1, column);
+		}
+		if(!checkNumbers(row, column+1)) {
+			removeLight(row, column+1);
+		}
+		if(!checkNumbers(row, column-1)) {
+			removeLight(row, column-1);
+		}		
+	}
+	
+	public boolean giveMeLight(int row, int column) {
+		checkNumbers(row, column);
+		return getBoard()[row][column];
+	}
+	
+	public void oneMovement() {
+		setMovements(getMovements()+1);
+	}
+	
+	//Privates methods and simple checks
+	
+	private void removeLight(int row, int column) {
+		if (!giveMeLight(row, column)) {
+			getBoard()[row][column] = true;
+		}
+		getBoard()[row][column] = false;
+	}
+
+	private void addLight(int row, int column) {
+		if (giveMeLight(row,column)) {
+			getBoard()[row][column] = false;
+		}
+		getBoard()[row][column] = true;
+	}
+
+	private boolean checkNumbers(int numberOne, int numberTwo) {		
+		return numberOne < 0 || numberTwo < 0 || 
+				numberOne > getBoard().length || numberTwo > getBoard().length;
+	}
+
+	private void checkSizeValue(int size) {
+		if(size < 4) {
+			throw new IllegalArgumentException("The board size, it cant be less than four. ");
+		}		
+	}
+		
+	//Getters and Setters
+
+	public boolean[][] getBoard() {
+		return board;
+	}
+
+	public void setBoard(boolean[][] board) {
+		this.board = board;
+	}
+
+	public int getMovements() {
+		return movements;
+	}
+
+	public void setMovements(int movements) {
+		if (movements < 0) {
+			throw new RuntimeException("The value of movement cant be less than zero. ");
+		}
+		this.movements = movements;
+	}
+	
+}
