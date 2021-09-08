2fe5928db704a1f0d77a400abceb31390f79f0e9
Cristian Gabriel <cristian_granero90@yahoo.com>
2021-09-08 20:37:20 -0300

    Some deleted comments

--- src/presenter/MainPresenter.java
+++ src/presenter/MainPresenter.java
@@ -15,50 +15,40 @@ public class MainPresenter implements Contract.Presenter{
 
 	@Override
 	public void onButtonLight(int posX, int posY) {
-		// TODO Auto-generated method stub
-		generateLights(posX, posY);
-		
+		generateLights(posX, posY);		
 	}
 
 	public void generateLights(int posX, int posY) {
-		// TODO Auto-generated method stub
 		model.generateLights(posX, posY);
 	}
 
 	@Override
 	public int boardSize() {
-		// TODO Auto-generated method stub
 		return model.getBoardSize();
 	}
 
 	@Override
 	public LightsOutGame board() {
-		// TODO Auto-generated method stub
 		return model.getAll();
 	}
 
 	@Override
 	public boolean haveLight(int x, int y) {
-		// TODO Auto-generated method stub
 		return model.giveMeLight(x, y);
 	}
 
 	@Override
 	public void updateLights(int posX, int posY, boolean status) {
-		// TODO Auto-generated method stub
 		view.changeLight(posX, posY, status);
 	}
 
 	@Override
 	public void updateMovements(int movements) {
-		// TODO Auto-generated method stub
-		view.updateMovements(movements);
-		
+		view.updateMovements(movements);		
 	}
 
 	@Override
 	public void oneMovement() {
-		// TODO Auto-generated method stub
 		model.oneMovement();
 	}
 
@@ -96,7 +86,6 @@ public class MainPresenter implements Contract.Presenter{
 
 	@Override
 	public void comunicateBuilding() {
-		// TODO Auto-generated method stub
 		model.setBuildBoard();
 	}
 }
